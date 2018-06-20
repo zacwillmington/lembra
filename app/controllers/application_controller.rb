04150f7062323c9499ff29ce84a435648f8ae748
Zac Willmington <zacwillmington@Zacs-Air.attlocal.net>
2018-06-19 20:00:03 -0700

    Create controllers configure block.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+require './config/environment'
+require 'bcrypt'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+end
