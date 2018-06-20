c08c9b1678a37feeef69fcef16cefadc55365899
Zac Willmington <zacwillmington@Zacs-Air.attlocal.net>
2018-06-20 15:33:10 -0700

    Hooked up deck controller actions for deck id and deck edit and deck show erb page.

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -1,3 +1,17 @@
 class DeckController < ApplicationController
 
+    get '/decks' do
+    
+    end
+
+    get '/decks/:id' do
+        @deck = Deck.find_by(:id => params[:id])
+        erb :'decks/show'
+    end
+
+    get '/decks/:id/edit' do
+
+    end
+
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,47 +5,51 @@
              @user = Helpers.current_user(session)
              redirect to "/users/#{@user.id}/decks"
          else
-             erb :'/user/signup'
+             erb :'/users/signup'
          end
      end
 
      post '/signup' do
-          binding.pry
           if Helpers.is_logged_in?(session)
               @user = Helpers.current_user(session)
               redirect to "/users/#{@user.id}/decks"
-        elsif Helpers.is_params_empty?(params)
-            redirect to '/signup'
-        else
-
+        elsif !Helpers.is_params_empty?(params)
             @user = User.create(:username => params['username'], :email => params['email'], :password => params['password'])
-            binding.pry
+            session[:id] = @user.id
+            redirect to "/users/#{@user.id}/decks"
+        else
+            redirect to '/signup'
+
+        end
+     end
+
+     get '/login' do
+         erb :'/users/login'
+     end
+
+     post '/login' do
+         @user = User.find_by(:username => params['username'])
+        if @user == nil
+            redirect to '/signup'
+        elsif @user.authenticate(params['password'])
             session[:id] = @user.id
             redirect to "/users/#{@user.id}/decks"
         end
      end
 
-     get '/login' do
-         erb :'/user/login'
-     end
-
-     post '/login' do
-         redirect to "/users/#{@user.id}/decks"
-     end
-
      get '/users/:id/decks' do
 
-         if Helpers.current_user(session).id == params[:id].to_i
-             @user = Helpers.current_user(session)
-             binding.pry
-             erb :'/user/show'
-         elsif Helpers.is_logged_in?(session)
-             binding.pry
+         if Helpers.is_logged_in?(session)
              @user = User.find_by(:id => params[:id])
-             erb :'/user/show'
+             erb :'/users/show'
          else
-             binding.pry
              redirect to '/login'
          end
+     end
+
+
+     get '/logout' do
+         session.clear
+         redirect to '/login'
      end
  end
