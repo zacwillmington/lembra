69497a0f23dbb65516353d4e0cf3ded1f1f97e3a
Zac Willmington <zacwillmington@Zacs-Air.attlocal.net>
2018-06-20 13:05:01 -0700

    renderd show page and showed all decks for user. Made helper method to count learnt cards to display on show page.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,28 @@
  class UserController < ApplicationController
 
      get '/signup' do
-         erb :'/user/signup'
+         if Helpers.is_logged_in?(session)
+             @user = Helpers.current_user(session)
+             redirect to "/users/#{@user.id}/decks"
+         else
+             erb :'/user/signup'
+         end
      end
 
      post '/signup' do
           binding.pry
-          redirect to :'/users/<%=@user.id%>/decks'
+          if Helpers.is_logged_in?(session)
+              @user = Helpers.current_user(session)
+              redirect to "/users/#{@user.id}/decks"
+        elsif Helpers.is_params_empty?(params)
+            redirect to '/signup'
+        else
+
+            @user = User.create(:username => params['username'], :email => params['email'], :password => params['password'])
+            binding.pry
+            session[:id] = @user.id
+            redirect to "/users/#{@user.id}/decks"
+        end
      end
 
      get '/login' do
@@ -14,11 +30,20 @@
      end
 
      post '/login' do
-         redirect to :'/users/<%=@user.id%>/decks'
+         redirect to "/users/#{@user.id}/decks"
      end
 
      get '/users/:id/decks' do
          binding.pry
-         erb :'/users/show'
+         @user = Helpers.current_user(session)
+         if @user.id == params[:id].to_i
+             erb :'/user/show'
+         elsif !Helpers.is_logged_in?(session)
+             redirect to '/login'
+         else
+             binding.pry
+             @user = User.Find_by(:id => params[:id])
+             erb :'/user/show'
+         end
      end
  end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,29 @@
 class Helpers
 
-    def self.is_logged_in?(session)
-        
-    end
-
     def self.current_user(session)
+        if session[:id]
+            @user = User.find(session[:id])
+        else
+            nil
+        end
     end
 
-    def self.has_params(params)
+    def self.is_logged_in?(session)
+        if session[:id] &&  self.current_user(session).id == session[:id]
+            true
+        else
+            false
+        end
+    end
+
+    def self.is_params_empty?(params)
+        params['username'] == "" || params['email'] == "" || params['password'] == "" || params['content'] == ""
+    end
+
+    def self.calculate_learnt_cards(deck)
+        learnt_cards = deck.cards.collect do |card|
+            card.learnt == true
+        end.size
     end
 
 end
