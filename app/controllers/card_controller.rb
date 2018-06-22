fd959d127228818474916a02889c9f428f6bd00d
Zac Willmington <zacwillmington@Zacs-Air.attlocal.net>
2018-06-21 19:56:25 -0700

    Made edit card controller action, fixed incorrect name of params['back_side'] value and added labels to card sides for show_deck.erb display.

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,14 +1,31 @@
 class CardController < ApplicationController
 
     get '/users/:id/decks/:deck_id/cards' do
-        erb :'/cards/cards'
+        @user = Helpers.current_user(session)
+        @deck = Deck.find_by(:id => params[:deck_id])
+        redirect to "/users/#{@deck.user.id}/decks/#{@deck.id}"
     end
 
     get '/users/:id/decks/:deck_id/cards/new' do
         binding.pry
         @user = Helpers.current_user(session)
         @deck = Deck.find_by(:id => params[:deck_id])
-        erb :'/cards/create_card'
+        if @deck.user.id == @user.id
+            erb :'/cards/create_card'
+        else
+            redirect to "/users/#{@user.id}/decks"
+        end
+    end
+
+    post '/users/:id/decks/:deck_id/cards' do
+        @user = Helpers.current_user(session)
+        @deck = Deck.find_by(:id => params[:deck_id])
+        @card = Card.create(:front_side => params['front_side'], :back_side => params['back_side'])
+        @deck.cards << @card
+        # @deck.number_of_cards = @deck.cards.all.size
+        @deck.save
+        binding.pry
+        redirect to "/users/#{@deck.user.id}/decks/#{@deck.id}/cards"
     end
 
     get '/users/:id/decks/:deck_id/cards/:card_id' do
